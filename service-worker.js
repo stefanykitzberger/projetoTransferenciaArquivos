9466d5abffb37449a30f7084d4495a18ffededfd
stefanykitzberger <kitzbergerstefany@gmail.com>
2025-08-28 21:25:06 -0300

    estruturando projeto

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "meu-pwa-cache-v1";
+const FILES_TO_CACHE = [
+    "index.html",
+    "sobre.html",
+    "style.css",
+    "manifest.json",
+    "icons/icon-192.png",
+    "icons/icon-512.png",
+    "icons/logo.jpg"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
